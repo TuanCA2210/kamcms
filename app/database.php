4442e8a9155b38333639c53c2f2988c97345be49
cuongledev <cuongle.dev@gmail.com>
2017-03-03 15:58:59 +0700

    Updated by cuongle 03-03-17

--- app/database.php
+++ app/database.php
@@ -5,8 +5,8 @@
 * 2016
 */
 $_LNC['db_host']      = 'localhost';
-$_LNC['db_user']      = 'cuong';
-$_LNC['db_password']  = 'aPn0~4f7';
+$_LNC['db_user']      = 'root';
+$_LNC['db_password']  = '';
 $_LNC['db_charset']   = 'utf8';
 $_LNC['db_name']      = 'core_db';
 $_LNC['db_port']      = null; 
